f0356d27a77535b64b3b7457785345811bab7c04
Ernest Buffington <ernest.buffington@gmail.com>
2017-09-20 06:21:16 -0400

    Add files via upload

---
+++ modules/Blog_Archive/language/lang-english.php
@@ -0,0 +1,40 @@
+<?php
+#########################################################################
+# Titanium Blog Archive v1.0                                            #
+#########################################################################
+# PHP-Nuke Titanium : Enhanced PHP-Nuke Web Portal System               #
+#########################################################################
+# [CHANGES]                                                             #
+# Table Header Module Fix by TheGhost               v1.0.0   01/30/2012 #
+# Nuke Patched                                      v3.1.0   06/26/2005 #
+#########################################################################
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+define("_PRINTER","Printer Friendly Page");
+define("_FRIEND","Send this Blog to a Friend");
+define("_ARTICLES","Blogs");
+define("_USCORE","Score");
+define("_DATE","Date");
+define("_SELECTMONTH2VIEW","<b>Please select the Blog Archive you wish to view :</b>");
+define("_SHOWALLSTORIES","Show ALL Blogs");
+define("_STORIESARCHIVE","Blogs Archive");
+define("_ACTIONS","Actions");
+define("_ARCHIVESINDEX","Blogs Archive Index");
+define("_ALLSTORIESARCH","All Blogs");
+define("_NEXTPAGE","Next Page");
+define("_PREVIOUSPAGE","Previous Page");
+?>
