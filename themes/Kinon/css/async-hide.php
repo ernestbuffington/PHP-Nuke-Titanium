975b1e99e464cd68a6329fa048914b0a03e487b8
Ernest Buffington <ernest.buffington@gmail.com>
2019-08-13 04:09:18 -0400

    PHP CSS Files for theme design on the fly.

---
+++ themes/Kinon/css/async-hide.php
@@ -0,0 +1,10 @@
+<?php
+/*=======================================================================
+ PHP-Nuke Titanium: Enhanced and Advanced
+ =======================================================================*/
+echo "/* Optimize page-hiding with CSS 3 code */\n";
+echo "/* Sets the opacity of any element with this class to 0 */\n";
+echo ".async-hide {\n";
+echo "opacity: 0 !important\n";
+echo "}\n\n";
+ ?>
