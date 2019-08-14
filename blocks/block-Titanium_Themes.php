a8456bddd7749a1d69dcbad4d2d8f1c66d127094
Ernest Buffington <ernest.buffington@gmail.com>
2019-08-13 22:17:20 -0400

    Titanium Themes block update.

--- blocks/block-Titanium_Themes.php
+++ blocks/block-Titanium_Themes.php
@@ -10,7 +10,7 @@
   
    Filename      : block-Titanium_Themes.php
    Author        : Ernest Buffington / JeFFb68CAM 
-   Websites      : (hub.86it,us)      /(www.Evo-Mods.com)
+   Websites      : (hub.86it.us)     / (www.Evo-Mods.com)
    Version       : 3.0.0
    Date          : 08.13.2019 (mm.dd.yyyy)
                                                                         
@@ -21,26 +21,26 @@ exit;
 
 global $module_name;
 
-if (is_user()):
-  if(empty($module_name)):
-   $module_name = 'News';
-endif;
-  $content  = "<div align=\"center\"><br />\n"; 
-  $content .= "<form action=\"modules.php?name=$module_name\" method=\"post\">";
-  $content .= "<input type=\"hidden\" name=\"chngtheme\" value=\"1\"/>\n";
-  $content .= "<input type=\"hidden\" name=\"user_id\" value=\"$userinfo[user_id]\">";
-  $content .= "<input type=\"hidden\" name=\"op\" value=\"savetheme\">";
-  $content .= GetThemeSelect('theme', 'user_themes', false, 'onChange=submit();');
-  $content .= "</form>\n"; 
-  $content .= "</div><br />\n";
+if(is_user()):
+	if(empty($module_name)):
+	  $module_name = main_module();
+	endif;
+    $content  = "<div align=\"center\"><br />\n"; 
+    $content .= "<form action=\"modules.php?name=$module_name\" method=\"post\">";
+    $content .= "<input type=\"hidden\" name=\"chngtheme\" value=\"1\"/>\n";
+	   $content .= "<input type=\"hidden\" name=\"user_id\" value=\"$userinfo[user_id]\">";
+	   $content .= "<input type=\"hidden\" name=\"op\" value=\"savetheme\">";
+    $content .= GetThemeSelect('theme', 'user_themes', false, 'onChange=submit();');
+    $content .= "</form>\n"; 
+    $content .= "</div><br />\n";
 else:
- if(empty($module_name)):
-  $module_name = 'News';
-endif;
-  $content  = "<div align=\"center\"><br />\n"; 
-  $content .= "<form action=\"modules.php?name=$module_name\" method=\"post\">";
-  $content .= GetThemeSelect('tpreview', 'user_themes', false, 'onChange=submit();', get_theme(), 0);
-  $content .= "</form>\n"; 
-  $content .= "</div><br />\n";
+	if(empty($module_name)):
+	  $module_name = main_module();
+	endif;
+    $content  = "<div align=\"center\"><br />\n"; 
+    $content .= "<form action=\"modules.php?name=$module_name\" method=\"post\">";
+    $content .= GetThemeSelect('tpreview', 'user_themes', false, 'onChange=submit();', get_theme(), 0);
+    $content .= "</form>\n"; 
+    $content .= "</div><br />\n";
 endif;
 ?>
