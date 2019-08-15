6c3f71122b1da6304858b1b942fe41be35c62bce
Ernest Buffington <ernest.buffington@gmail.com>
2019-08-15 02:50:55 -0400

    Add files via upload

---
+++ modules/News/language/lang-english.php
@@ -0,0 +1,156 @@
+<?php
+/*=======================================================================
+ PHP-Nuke Titanium v3.0.0 : Enhanced PHP-Nuke Web Portal System
+ =======================================================================*/
+
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+
+/*****[CHANGES]**********************************************************
+-=[Mod]=-
+      NSN Blogs                                 v1.1.0       06/25/2005
+ ************************************************************************/
+
+define('_NEWS','Blogs');
+
+
+define("_SEND","Send");
+define("_URL","URL");
+define("_PRINTER","Printer Friendly");
+define("_FRIEND","Send to a Friend");
+define("_YOURNAME","Your Name");
+define("_OK","Ok!");
+define("_RELATED","Related Links");
+define("_MOREABOUT","More about");
+define("_NEWSBY","Blogs by");
+define("_MOSTREAD","Most read Blog");
+define("_READMORE","Read More...");
+define("_BYTESMORE","bytes more");
+define("_COMMENTSQ","comments?");
+define("_COMMENT","comment");
+define("_CONFIGURE","Configure");
+define("_LOGINCREATE","Login/Create an Account");
+define("_THRESHOLD","Threshold");
+define("_NOCOMMENTS","No Comments");
+define("_NESTED","Nested");
+define("_FLAT","Flat");
+define("_THREAD","Thread");
+define("_OLDEST","Oldest First");
+define("_NEWEST","Newest First");
+define("_HIGHEST","Highest Scores First");
+define("_COMMENTSWARNING","The comments are owned by the poster. We aren't responsible for their content.");
+define("_SCORE","Score:");
+define("_USERINFO","User Info");
+define("_READREST","Read the rest of this comment...");
+define("_REPLY","Reply to This");
+define("_REPLYMAIN","Post Comment");
+define("_NOSUBJECT","No Subject");
+define("_NOANONCOMMENTS","No Comments Allowed for Anonymous, please <a href=\"modules.php?name=Your_Account&amp;op=new_user\">register</a>");
+define("_PARENT","Parent");
+define("_ROOT","Root");
+define("_UCOMMENT","Comment");
+define("_ALLOWEDHTML","Allowed HTML:");
+define("_POSTANON","Post Anonymously");
+define("_EXTRANS","Extrans (html tags to text)");
+define("_HTMLFORMATED","HTML Formatted");
+define("_PLAINTEXT","Plain Old Text");
+define("_ONN","on...");
+define("_SUBJECT","Subject");
+define("_COMMENTREPLY","Comment Post");
+define("_COMREPLYPRE","Comment Post Preview");
+define("_NOTRIGHT","Something is not right with passing a variable to this function. This message is just to keep things from messing up down the road");
+define("_SENDAMSG","Send a Message");
+define("_YOUSENDSTORY","You will send the story");
+define("_TOAFRIEND","to a specified friend:");
+define("_FYOURNAME","Your Name:");
+define("_FYOUREMAIL","Your E-mail:");
+define("_FFRIENDNAME","Your Friend's Name:");
+define("_FFRIENDEMAIL","Your Friend's E-mail:");
+define("_INTERESTING","Interesting Blog at");
+define("_YOURFRIEND","Your Friend");
+define("_CONSIDERED","considered the following Blog interesting and wanted to send it to you.");
+define("_FDATE","Date:");
+define("_FTOPIC","Topic:");
+define("_YOUCANREAD","You can read interesting Blogs at");
+define("_FSTORY","Story");
+define("_HASSENT","Has been sent to");
+define("_THANKS","Thanks!");
+define("_RECOMMEND","Recommend this Site to a Friend");
+define("_PDATE","Date:");
+define("_PTOPIC","Topic:");
+define("_COMESFROM","This Blog comes from");
+define("_THEURL","The URL for this story is:");
+define("_PREVIEW","Preview");
+define("_NEWUSER","New User");
+define("_OPTIONS","Options");
+define("_REFRESH","Refresh");
+define("_NOCOMMENTSACT","Sorry, Comments are not available for this Blog.");
+define("_ARTICLEPOLL","Blog's Poll");
+define("_RATEARTICLE","Blog Rating");
+define("_RATETHISARTICLE","Will you take a moment to vote for this Blog? Your VOTES are appreciated by the Author!");
+define("_CASTMYVOTE","Cast my Vote!");
+define("_AVERAGESCORE","Star Rating");
+define("_BAD","Bad");
+define("_REGULAR","Regular");
+define("_GOOD","Good");
+define("_VERYGOOD","Very Good");
+define("_EXCELLENT","Excellent");
+define("_ARTICLERATING","Blog Rating");
+define("_THANKSVOTEARTICLE","Thanks for voting for this Blog!");
+define("_ALREADYVOTEDARTICLE","Sorry, you already voted for this Blog recently!");
+define("_BACKTOARTICLEPAGE","Back to Blog's Page");
+define("_DIDNTRATE","You didn't select any score for the Blog!");
+define("_NOINFO4TOPIC","Sorry, there isn't information for the selected topic.");
+define("_GOTONEWSINDEX","Go to Blogs Index");
+define("_SELECTNEWTOPIC","Select a New Topic");
+define("_GOTOHOME","Go to Home");
+define("_SEARCHONTOPIC","Search on This Topic");
+define("_SEARCHDIS","Search Discussion");
+define("_READPDF","Read as PDF");
+define("_READWITHCOMMENTS", "You can read the complete story with its comments from");
+/*****[BEGIN]******************************************
+ [ Mod:     NSN Blogs                           v1.1.0 ]
+ ******************************************************/
+define("_NE_SELECT","Select Page");
+define("_NE_OF","of");
+define("_NE_PAGES","pages");
+define("_NE_NEWSCONFIG","Blogs Configuration");
+define("_NE_DISPLAYTYPE","Display Column");
+define("_NE_SINGLE","Single Column");
+define("_NE_DUAL","Dual Column");
+define("_NE_READLINK","Read More Link");
+define("_NE_POPUP","Popup");
+define("_NE_PAGE","Page");
+define("_NE_TEXTTYPE","Blog Length");
+define("_NE_TRUNCATE","Truncate to 255 chars");
+define("_NE_COMPLETE","Original text");
+define("_NE_NOTIFYAUTH","Notify Author");
+define("_NE_NOTIFYAUTHNOTE","This will email Blog submitter<br />\non approval");
+define("_NE_NO","No");
+define("_NE_YES","Yes");
+define("_NE_HOMETOPIC","Topic in Home");
+define("_NE_ALLTOPICS","All Topics");
+define("_NE_HOMENUMBER","Blogs in Home");
+define("_NE_NUKEDEFAULT","Nuke Default");
+define("_NE_ARTICLES","Blogs");
+define("_NE_HOMENUMNOTE","This will override user preferences<br />\nif set other than Nuke Default");
+define("_NE_SAVECHANGES","Save Changes");
+/*****[END]********************************************
+ [ Mod:     NSN Blogs                           v1.1.0 ]
+ ******************************************************/
+define("_GFX_FAILURE","Please enter the correct GFX code");
+?>
