9c6dd7a5cbf843310fb5d860740599167f115748
Ernest Buffington <ernest.buffington@gmail.com>
2019-08-17 23:14:59 -0400

    Add files via upload

--- themes/CarbonFiber/function_FormatStory.php
+++ themes/CarbonFiber/function_FormatStory.php
@@ -1,34 +1,38 @@
 <?php
-/*----------------------------------------------------------------*/
-/* THEME INFO                                                     */
-/* CarbinFiber Theme v1.0 (Fixed & Full Width)                    */
-/*                                                                */
-/* A Very Nice Black Carbin Fiber Styled Design.                  */
-/* Copyright © 2019 By: TheGhost AKA Ernest Allen Bffington       */
-/* e-Mail : ernest.buffington@gmail.com                           */
-/*----------------------------------------------------------------*/
-/* CREATION INFO                                                  */
-/* Created On: 1st August, 2019 (v1.0)                            */
-/*                                                                */
-/* Updated On: 1st August, 2019 (v3.0)                            */
-/* HTML5 Theme Code Updated By: Lonestar (Lonestar-Modules.com)   */
-/*                                                                */
-/* Read CHANGELOG File for Updates & Upgrades Info                */
-/*                                                                */
-/* Designed By: TheGhost                                          */
-/* Web Site: https://theghost.86it.us                             */
-/* Purpose: PHP-Nuke Evolution | Titanium Edition                 */
-/*----------------------------------------------------------------*/
-/* CMS INFO                                                       */
-/* PHP-Nuke Copyright (c) 2005 by Francisco Burzi phpnuke.org     */
-/* Nuke-Evolution Basic: Enhanced PHP-Nuke Web Portal System      */
-/*----------------------------------------------------------------*/
+#---------------------------------------------------------------------------------------#
+# function FormatStory                                                                  #
+#---------------------------------------------------------------------------------------#
+# THEME INFO                                                                            #
+# CarbinFiber Theme v1.0 (Fixed & Full Width)                                           #
+#                                                                                       #
+# Final Build Date 08/17/2019 Saturday 7:40pm                                           #
+#                                                                                       #
+# A Very Nice Black Carbin Fiber Styled Design.                                         #
+# Copyright © 2019 By: TheGhost AKA Ernest Allen Bffington                              #
+# e-Mail : ernest.buffington@gmail.com                                                  #
+#---------------------------------------------------------------------------------------#
+# CREATION INFO                                                                         #
+# Created On: 1st August, 2019 (v1.0)                                                   #
+#                                                                                       #
+# Updated On: 1st August, 2019 (v3.0)                                                   #
+# HTML5 Theme Code Updated By: Lonestar (Lonestar-Modules.com)                          #
+#                                                                                       #
+# Read CHANGELOG File for Updates & Upgrades Info                                       #
+#                                                                                       #
+# Designed By: TheGhost                                                                 #
+# Web Site: https://theghost.86it.us                                                    #
+# Purpose: PHP-Nuke Titanium | Xtreme Evo                                               #
+#---------------------------------------------------------------------------------------#
+# CMS INFO                                                                              #
+# PHP-Nuke Copyright (c) 2006 by Francisco Burzi phpnuke.org                            #
+# PHP-Nuke Titanium (c) 2019 : Enhanced PHP-Nuke Web Portal System                      #
+#---------------------------------------------------------------------------------------#
 
 if (realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) 
     exit('Access Denied');
 	
 /*--------------------------*/
-/* 
+/* function FormatStory
 /*--------------------------*/
 function FormatStory($thetext, $notes, $aid, $informant) 
 {
