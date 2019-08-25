85e7081639ea53e924dd6a967072aff9111e527d
Ernest Buffington <ernest.buffington@gmail.com>
2019-08-25 19:44:28 -0400

    Feed Update
    Feed Update

--- versions/feed.php
+++ versions/feed.php
@@ -5,7 +5,20 @@
 <title>Live Feed from PHP-Nuke Titanium Hub</title>
 </head>
 <body> 
-<strong>New PHP-Nuke Titanium GitHub Repsoitory setup Sept 4, 2017 </strong><br />
+<strong>AUGUST 2019</strong>
+<hr>
+<strong>Changed Admin Live Feed : Using Curl like it should be - Aug 12, 2019 </strong><br />
+<strong>Web Links Module : added new reCAPTCHA - Aug 11, 2019 </strong><br />
+<strong>TegoNuke(tm)/NSN GR Downloads (NSNGD): added new reCAPTCHA - Aug 10, 2019 </strong><br />
+<strong>TegoNuke(tm)/NSN GR Downloads (NSNGD): now functional in PHP 7.3.6 - Aug 9, 2019 </strong><br />
+<strong>Finished CarbinFiber Red Flames Theme - Aug 9, 2019 </strong><br />
+<strong>Finished CarbinFiber Blue Skulls Theme - Aug 9, 2019 </strong><br />
+<strong>Ported Over Titanium Theme - Aug 9, 2019 </strong><br />
+<strong>Portal Menu v5.01 is now functional in PHP 7.3.6 - Aug 8, 2019 </strong><br /><br />
+
+<strong>SEPTEMBER 2017</strong>
+<hr>
+<strong>New PHP-Nuke Titanium GitHub Repsoitory setup - Sept 4, 2017 </strong><br />
 <a href="https://github.com/ernestbuffington/PHP-Nuke-Titanium" target="_blank">https://github.com/ernestbuffington/PHP-Nuke-Titanium</a>
 <hr>
 </body>
